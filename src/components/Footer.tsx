e698933a21df412eac3b309664c741d39c1a684e
aasim-syed <syedaasim133@gmail.com>
2025-11-02 16:18:13 +0530

    n train fix, refresh feature added

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,2 +1,2 @@
 import React from "react";
-export function Footer(){ return <div className="small">100% frontend. Toggle configs, train in a Worker, cache in IndexedDB, visualize recall vs latency.</div>; }
+export function Footer(){ return <div className="small">100% frontend. Toggle configs, train in a Worker, cache in IndexedDB, visualize recall vs latency. Built with ❤️ by <a href="https://github.com/aasim-syed" target="_blank" >github.com/aasim-syed</a> </div>; }
